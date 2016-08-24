e436049421880e367741a5eeabaa54c2da8cbb5c
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2016-08-24 15:45:10 -0300

    alteracoes de storyboard

--- Betting/RandomLetterViewController.swift
+++ Betting/RandomLetterViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class RandomLetterViewController: UIViewController {
+class RandomLetterViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var firstField: UITextField!
+    @IBOutlet weak var lastField: UITextField!
+    let limitLength = 1
     
     override func prefersStatusBarHidden() -> Bool { return true }
     
     @IBAction func closeButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstField.delegate = self
+        lastField.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.characters.count + string.characters.count - range.length
+        return newLength <= limitLength
     }
 
     override func didReceiveMemoryWarning() {
